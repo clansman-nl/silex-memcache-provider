d502a7dc9bd23a5a3103d41920e75bd8ebaf6ea8
Sebastiaan Hilbers <bashilbers@gmail.com>
2017-06-22 14:39:41 +0200

    initial

---
+++ src/MemcacheProvider.php
@@ -0,0 +1,38 @@
+s<?php
+
+namespace Basebuilder\MemcacheProvider;
+
+use Pimple;
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class MemcacheProvider implements ServiceProviderInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function register(Application $app)
+    {
+        $app['memcache'] = $app->share(function (Pimple $app) {
+            $defaults = [
+                'host' => '127.0.0.1',
+                'port' => 11211
+            ];
+
+            $opts = array_merge($defaults, (array) $app['memcache.options']);
+
+            $memcache = new \Memcache();
+            $memcache->connect($opts['host'], $opts['port']);
+
+            return $memcache;
+        });
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function boot(Application $app)
+    {
+        // TODO: Implement boot() method.
+    }
+}
